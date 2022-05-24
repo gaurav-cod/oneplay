4fb6f21048aad7344ed9c7f545cd239a0e0686a2
maynak <mainak@oneplay.in>
2022-05-24 21:00:50 +0530

    qqeer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   client_api: "https://client-apis.oneplay.in/services/v1",
   render_mix_api: "https://rendermix.oneplay.in/v1",
-  socket_endpoint: "https://rendermix.oneplay.in",
+  socket_endpoint: "https://chatservices.oneplay.in",
   game_assets: "https://cdn.edge-net.co/game_assets/",
   ga_tracking_id: "UA-220923953-2",
   firebase: {
