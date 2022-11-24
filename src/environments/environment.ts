499b2a452e94c651b62bb865b55764dabceca90f
BurnedOx666 <mainak@oneplay.in>
2022-11-24 20:30:31 +0530

    UI bugs fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  client_api: "https://client-apis.oneplay.in/services/v1",
+  client_api: "https://client-apis.oneream.com/services/v1",
   render_mix_api: "https://rendermix.oneream.com/v1",
   socket_endpoint: "https://communication-services.oneream.com",
   game_assets:
