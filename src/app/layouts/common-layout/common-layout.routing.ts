e9c9570c4acc2e4eb8da89a92e0255b0575e1de8
BurnedOx666 <mainak@oneplay.in>
2023-09-11 16:23:46 +0530

    move speed-test out of auth

--- src/app/layouts/common-layout/common-layout.routing.ts
+++ src/app/layouts/common-layout/common-layout.routing.ts
@@ -3,9 +3,11 @@ import { ErrorComponent } from "src/app/pages/error/error.component";
 import { ServerErrorComponent } from "src/app/pages/server-error/server-error.component";
 import { InstallComponent } from "src/app/pages/install/install.component";
 import { InstallGuard } from "src/app/guards/install.guard";
+import { SpeedTestComponent } from "src/app/pages/speed-test/speed-test.component";
 
 export const CommonLayoutRoutes: Routes = [
   { path: "error", component: ErrorComponent },
   { path: "server-error", component: ServerErrorComponent },
   { path: "install", component: InstallComponent, canActivate: [InstallGuard] },
+  { path: "speed-test", component: SpeedTestComponent },
 ];
