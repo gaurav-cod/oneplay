679a9c0e868f7c7cfe3b75fd6d07073884992e50
Gaurav Sahu <gaurav.sahu@isthara.com>
2023-11-16 14:03:19 +0530

    genreal setting done

--- src/app/components/general-settings/general-settings.component.ts
+++ src/app/components/general-settings/general-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from "src/environments/environment";
 
 @Component({
@@ -9,7 +10,25 @@ import { environment } from "src/environments/environment";
 export class GeneralSettingsComponent {
 
   appVersion: string = environment.appVersion;
-  constructor() {
+  constructor(
+    private readonly router: Router
+  ) { }
+
+  generalSettingRedirection(type: "FAQ" | "TERMS" | "SUPPORT" | "PRIVACY_POLICY") {
+    switch (type) {
+      case 'FAQ':
+        window.open(environment.domain + '/FAQs.html');
+        break;
+      case 'TERMS':
+        window.open(environment.domain + '/tnc.html');
+        break;
+      case 'SUPPORT':
+        // window.open(environment.domain + '/FAQs.html');
+        break;
+      case "PRIVACY_POLICY":
+        window.open(environment.domain + '/privacy.html');
+        break;
+    }
   }
 
 }
