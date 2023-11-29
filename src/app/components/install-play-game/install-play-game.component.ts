871a1a68faa90d48bb9c07ca6740214eb322c487
Gaurav Sahu <gaurav.sahu@letsmobility.com>
2023-11-29 15:18:34 +0530

    addition variable removed

--- src/app/components/install-play-game/install-play-game.component.ts
+++ src/app/components/install-play-game/install-play-game.component.ts
@@ -1,12 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { GameModel } from 'src/app/models/game.model';
 import { GLinkPipe } from 'src/app/pipes/glink.pipe';
 import { CountlyService } from 'src/app/services/countly.service';
 import { getGameLandingViewSource } from 'src/app/utils/countly.util';
-import { environment } from 'src/environments/environment';
-import { v4 } from 'uuid';
 
 @Component({
   selector: 'app-install-play-game',
@@ -17,16 +14,9 @@ import { v4 } from 'uuid';
 export class InstallPlayGameComponent {
   @Input("game") game: GameModel;
   @Input("queryParams") queryParams?: any;
-  @Input("hasFixedWidth") hfw: boolean = false;
   @Input("showBottomLabel") showBottomLabel: boolean = true;
   @Input('calledFrom') calledFrom: "HOME" | "STORE_INSTALL_PLAY" | "STORE_OTHER" = "HOME";
 
-  timer: NodeJS.Timeout;
-  muted = true;
-  showSound = false;
-  showTitle = false;
-
-  readonly loaderId = v4();
 
   get isMobile() {
     return window.innerWidth < 768;
@@ -35,7 +25,6 @@ export class InstallPlayGameComponent {
   constructor(
     private readonly router: Router,
     private readonly gLink: GLinkPipe,
-    private readonly loaderService: NgxUiLoaderService,
     private readonly countlyService: CountlyService
   ) { }
 
