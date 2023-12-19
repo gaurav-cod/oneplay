4a8b3604c5cb2ce484b5e450db423b9b5e59ca3f
Gaurav Sahu <gaurav.sahu@letsmobility.com>
2023-12-19 13:24:54 +0530

    Internal Routing issue resolved

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -23,8 +23,40 @@ messaging.onBackgroundMessage(function (payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: 'assets/img/brand/brandLogo.svg'
+    icon: 'assets/img/brand/brandLogo.svg',
+    image: 'assets/img/brand/brandLogo.svg',
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
+messaging.clickedNotification(function (payload) {
+  console.log("Received background message on click", payload);
+  let navigationString = environment.domain;
+
+  switch (payload.data?.sub_type) {
+    case "SUBSCRIPTION_EXPIRING":
+    case "SUBSCRIPTION_EXPIRED":
+    case "LIMITED_TOKEN_REMAIN":
+      navigationString = environment.domain + "/subscription.html";
+      break;
+    case "UNUSUAL_ACCOUNT_ACTIVITY":
+      navigationString = environment.domain + "/dashboard/settings/security";
+      break;
+    case "PAYMENT_FAILED":
+    case "PAYMENT_SUCCESS":
+    case "FRIEND_REQUEST":
+    case "SCHEDULED_MAINTENANCE":
+    case "WELCOME_MESSAGE":
+    case "SCHEDULED_MAINTENANCE":
+    case "NEW_GAMES_AVAILABLE":
+    case "GAME_UPDATE_AVAILABLE":
+    case "DISCOUNT_OFFER":
+    case "PASSWORD_CHANGE":
+      navigationString = environment.domain + "/dashboard";
+      break;
+  }
+
+  clients.openWindow(navigationString);
+})
+
