75f2f15815717fa0be20f5aabdb6a8382d3ad194
BurnedOx666 <mainak@oneplay.in>
2023-12-21 13:03:41 +0530

    background notification changes

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,37 +1,25 @@
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+importScripts("firebase-config.js");
 
-// Initialize the Firebase app in the service worker by passing the generated config
-var firebaseConfig = {
-  apiKey: "AIzaSyD37MW-f2U9q1MZK-aN49ZHXJDQFGGjjzA",
-  authDomain: "oneplayapp-2ce5f.firebaseapp.com",
-  projectId: "oneplayapp-2ce5f",
-  storageBucket: "oneplayapp-2ce5f.appspot.com",
-  messagingSenderId: "288317360848",
-  appId: "1:288317360848:web:e23276f437f94102256cd5",
-  measurementId: "G-HPVSR3Y6Z5",
-};
-
-firebase.initializeApp(firebaseConfig);
+const environment = self.config;
+firebase.initializeApp(environment.firebaseConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
-  console.log("Received background message ", payload);
-
   const notificationTitle = payload.notification.body;
   const notificationOptions = {
     body: payload.data.title,
-    icon: 'assets/img/brand/brandLogo.svg',
-    image: 'assets/img/brand/brandLogo.svg',
+    icon: environment.domain + '/dashboard/assets/img/brand/brandLogo.svg',
+    image: environment.domain + '/dashboard/assets/img/brand/brandLogo.svg',
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
 self.addEventListener("notificationclick", function (payload) {
-  console.log("Received background message on click 1", payload);
   const clickedNotification = payload.notification;
   let navigationString = environment.domain;
 
@@ -61,35 +49,3 @@ self.addEventListener("notificationclick", function (payload) {
   clients.openWindow(navigationString);
   clickedNotification.close();
 });
-
-
-// messaging.clickedNotification(function (payload) {
-//   console.log("Received background message on click", payload);
-//   let navigationString = environment.domain;
-
-//   switch (payload.data?.sub_type) {
-//     case "SUBSCRIPTION_EXPIRING":
-//     case "SUBSCRIPTION_EXPIRED":
-//     case "LIMITED_TOKEN_REMAIN":
-//       navigationString = environment.domain + "/subscription.html";
-//       break;
-//     case "UNUSUAL_ACCOUNT_ACTIVITY":
-//       navigationString = environment.domain + "/dashboard/settings/security";
-//       break;
-//     case "PAYMENT_FAILED":
-//     case "PAYMENT_SUCCESS":
-//     case "FRIEND_REQUEST":
-//     case "SCHEDULED_MAINTENANCE":
-//     case "WELCOME_MESSAGE":
-//     case "SCHEDULED_MAINTENANCE":
-//     case "NEW_GAMES_AVAILABLE":
-//     case "GAME_UPDATE_AVAILABLE":
-//     case "DISCOUNT_OFFER":
-//     case "PASSWORD_CHANGE":
-//       navigationString = environment.domain + "/dashboard";
-//       break;
-//   }
-
-//   clients.openWindow(navigationString);
-// })
-
