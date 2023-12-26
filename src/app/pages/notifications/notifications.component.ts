2cf185c9b34bb80129d1d4a59af47949628c7fe2
Gaurav Sahu <gaurav.sahu@letsmobility.com>
2023-12-26 19:12:48 +0530

    Ticket 1164 Resolved

--- src/app/pages/notifications/notifications.component.ts
+++ src/app/pages/notifications/notifications.component.ts
@@ -91,7 +91,7 @@ export class NotificationsComponent implements OnInit {
         if (notification.subType === "SUBSCRIPTION_EXPIRING")
           this.checkoutPageOfPlan(notification);
         else
-          this.renewSubscription();
+          this.renewSubscription(notification);
         break;
       case "RESET_PASSWORD":
         this.router.navigate(['/settings/security']);
@@ -105,34 +105,25 @@ export class NotificationsComponent implements OnInit {
   checkoutPageOfPlan(notifiaction) {
     this.router.navigate([`/checkout/${notifiaction.data.subscription_package_id}`]);
   }
-  renewSubscription() {
-    this.restService.getCurrentSubscription().subscribe({
-      next: (response) => {
-        let plan = '';
-        if (response.length > 0) {
-          if (response[0].totalTokenOffered <= 60) {
-            plan = '60';
-          } else if (response[0].totalTokenOffered <= 180) {
-            plan = '180';
-          } else if (response[0].totalTokenOffered <= 300) {
-            plan = '300';
-          } else if (response[0].totalTokenOffered <= 600) {
-            plan = '600';
-          } else if (response[0].totalTokenOffered <= 1200) {
-            plan = '1200';
-          } else {
-            plan = '10800';
-          }
+  renewSubscription(notifiaction) {
+    
+        let plan = '10800';
+        if (notifiaction.data?.offered_tokens <= 60) {
+          plan = '60';
+        } else if (notifiaction.data?.offered_tokens <= 180) {
+          plan = '180';
+        } else if (notifiaction.data?.offered_tokens <= 300) {
+          plan = '300';
+        } else if (notifiaction.data?.offered_tokens <= 600) {
+          plan = '600';
+        } else if (notifiaction.data?.offered_tokens <= 1200) {
+          plan = '1200';
+        } else {
+          plan = '10800';
         }
+        
         window.open(environment.domain + `/subscription.html?plan=${plan}`, '_self');
-      }, error: (err) => {
-        Swal.fire({
-          icon: "error",
-          title: "Error Code: " + err.code,
-          text: err.message,
-        });
-      }
-    })
+
   }
 
   toggleNotificationActionBtn(notificationDetail) {
