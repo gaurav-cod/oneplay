638ca81ab317122b38d77899101475a0254d5f22
maynak <mainak@oneplay.in>
2024-01-31 23:35:48 +0530

    call set preffered store if logged in

--- src/app/pages/view/view.component.ts
+++ src/app/pages/view/view.component.ts
@@ -1507,14 +1507,16 @@ export class ViewComponent implements OnInit, OnDestroy {
   selectStore(store: PurchaseStore) {
     if (!this.selectedStore || this.selectedStore.name !== store.name) {
       this.selectedStore = store;
-      lastValueFrom(
-        this.restService.setPreferredStoreForGame(
-          this.game.oneplayId,
-          store.name
-        )
-      ).catch((error)=> {
-        this.showError(error);
-      });
+      if (this.authService.sessionToken) {
+        lastValueFrom(
+          this.restService.setPreferredStoreForGame(
+            this.game.oneplayId,
+            store.name
+          )
+        ).catch((error)=> {
+          this.showError(error);
+        });
+      }
     }
   }
 
