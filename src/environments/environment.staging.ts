78149f1412476a143568929141751525aee873d7
ajay <ajay@oneplay.in>
2024-02-01 06:41:24 +0000

    Version changes to 6.0.0

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  appVersion: "5.8.0",
+  appVersion: "6.0.0",
   production: true,
   cookie_domain: "qa.oneream.com",
   domain: "https://qa.oneream.com",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // import { version } from '../../package.json';
 
 export const environment = {
-  appVersion: "5.8.0",
+  appVersion: "6.0.0",
   production: false,
   cookie_domain: "localhost",
   domain: "http://localhost:4200",
