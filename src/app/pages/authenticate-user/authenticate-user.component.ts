a1fca7ca3ac30607b008ee114148ee2876f3042c
Gaurav Sahu <gaurav.sahu@letsmobility.com>
2024-02-20 10:43:42 +0530

    hotfix issue resolved

--- src/app/pages/authenticate-user/authenticate-user.component.ts
+++ src/app/pages/authenticate-user/authenticate-user.component.ts
@@ -475,6 +475,14 @@ export class AuthenticateUserComponent implements OnInit, OnDestroy, AfterViewIn
     }
   }
 
+  onKeyPressCheckMobile(event: KeyboardEvent) {
+    const charCode = event.charCode;
+    const validChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-_+@';
+    if (validChars.includes(String.fromCharCode(charCode)) || event.code == "KeyE") {
+      event.preventDefault();
+    }
+  }
+
   private handlePaste(event: ClipboardEvent) {
     event.stopPropagation();
 
