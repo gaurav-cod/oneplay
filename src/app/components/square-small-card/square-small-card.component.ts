bc6915177773277e09d6da255e02ff606c13e9c2
Gaurav Sahu <gaurav.sahu@letsmobility.com>
2024-03-13 18:38:40 +0530

    category small rail video done

--- src/app/components/square-small-card/square-small-card.component.ts
+++ src/app/components/square-small-card/square-small-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { GameModel } from 'src/app/models/game.model';
+import { environment } from 'src/environments/environment';
+import { v4 } from 'uuid';
 
 @Component({
   selector: 'app-square-small-card',
@@ -10,6 +13,87 @@ export class SquareSmallCardComponent implements OnInit {
 
   @Input() game:GameModel;
 
+  timer: NodeJS.Timeout;
+  muted = true;
+  showSound = false;
+  showTitle = false;
+  imageLoaded = false;
+
+  constructor(
+    private readonly loaderService: NgxUiLoaderService
+  ) {}
+
+  readonly loaderId = v4();
+
+  get isMobile() {
+    return window.innerWidth < 768;
+  }
+
   ngOnInit(): void {
+  }
+
+  playVideo(gameLink: HTMLAnchorElement, image: HTMLImageElement) {
+    if (this.game.trailer_video && !this.isMobile && this.game.status === "live") {
+      this.timer = setTimeout(() => {
+        this.showSound = true;
+        if (!(gameLink.firstElementChild instanceof HTMLVideoElement)) {
+          const video = document.createElement("video");
+          gameLink.insertAdjacentElement("afterbegin", video);
+          video.classList.add("mask");
+          video.src =
+            environment.game_assets +
+            this.game.oneplayId +
+            this.game.trailer_video;
+          video.muted = true;
+          video.style.objectFit = 'cover';
+          video.style.width = "100%";
+          video.style.height = "150%";
+          video.style.zIndex = "100000";
+          video.style.border = "2px solid transparent";
+          video.style.backgroundImage = "linear-gradient(to bottom right, #FF0CF5, #fc77f8, #0575E6, #0575E6, #0575E6)";
+          video.style.backgroundOrigin = "border-box";
+
+          if (video.getBoundingClientRect().right > window.innerWidth) {
+            video.style.left = String(Number(video.style.left) - 250) + "px";
+          }
+
+          video.play();
+          // circular loader until video is loaded
+          this.loaderService.startLoader(this.loaderId);
+          video.onloadeddata = () => {
+            this.loaderService.stopLoader(this.loaderId);
+          };
+        }
+      }, 1000);
+    }
+  }
+
+  pauseVideo(gameLink: HTMLAnchorElement, image: HTMLImageElement) {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    if (this.game.trailer_video && !this.isMobile && this.game.status === "live") {
+      image.style.opacity = "1";
+      if (gameLink.firstElementChild instanceof HTMLVideoElement) {
+        gameLink.removeChild(gameLink.firstElementChild);
+      }
+      this.showSound = false;
+      this.muted = true;
+      this.loaderService.stopLoader(this.loaderId);
+    }
+  }
+
+  muteUnmute(e: Event, gameLink: HTMLAnchorElement, game: GameModel) {
+    e.stopPropagation();
+    if (game.trailer_video) {
+      const video = gameLink.firstElementChild as HTMLVideoElement;
+      if (video.muted) {
+        video.muted = false;
+        this.muted = false;
+      } else {
+        video.muted = true;
+        this.muted = true;
+      }
+    }
   }
 }
