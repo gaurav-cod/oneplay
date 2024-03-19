aaac35941a9679e90a6c955dddc7c6761562fcd2
Gaurav Sahu <gaurav.sahu@letsmobility.com>
2024-03-19 18:48:20 +0530

    scroll issue resolved

--- src/app/components/category-small-rail/category-small-rail.component.ts
+++ src/app/components/category-small-rail/category-small-rail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { GameModel } from 'src/app/models/game.model';
 import { GameFeedModel } from 'src/app/models/gameFeed.model';
 import { GamezopModel } from 'src/app/models/gamezop.model';
@@ -10,7 +10,7 @@ import { RestService } from 'src/app/services/rest.service';
   templateUrl: './category-small-rail.component.html',
   styleUrls: ['./category-small-rail.component.scss']
 })
-export class CategorySmallRailComponent implements OnInit, OnDestroy {
+export class CategorySmallRailComponent implements OnInit, AfterViewInit, OnDestroy {
 
 
   constructor(
@@ -19,12 +19,17 @@ export class CategorySmallRailComponent implements OnInit, OnDestroy {
 
   @Input() gameFeed: GamezopFeedModel;
   @Input() contentId: string;
+  @ViewChild("container") containerRef: ElementRef<HTMLDivElement>;
+  
   public gamesListBatches = {};
 
 
   public isFilterApplied: boolean = false;
   public selectedFilter: string = "All";
   public isLoading: boolean = false;
+
+  public showRightArrow = false;
+  public showLeftArrow = false;
 
   private _arrowTimeout: NodeJS.Timer;
   private _loaderTimeout: NodeJS.Timer;
@@ -40,6 +45,10 @@ export class CategorySmallRailComponent implements OnInit, OnDestroy {
       this.gameFeed.categories.splice(0, 0, "All");
   }
 
+  ngAfterViewInit(): void {
+    this._arrowTimeout = setTimeout(() => this.updateArrows(), 100);
+  }
+
   rearrangeGameBatch(games: GamezopModel[]) {
     this.gamesListBatches = {};
     let key: number = -1;
@@ -52,6 +61,39 @@ export class CategorySmallRailComponent implements OnInit, OnDestroy {
       this.gamesListBatches[key].push(games[i]);
     }
   }
+
+  scrollRight() {
+    const container = this.containerRef.nativeElement;
+    let scrollAmount = 0;
+    const slideTimer = setInterval(() => {
+      container.scrollLeft += container.clientWidth / 12;
+      scrollAmount += container.clientWidth / 12;
+      if (scrollAmount >= container.clientWidth / 2) {
+        window.clearInterval(slideTimer);
+        this.updateArrows();
+      }
+    }, 25);
+  }
+
+  scrollLeft() {
+    const container = this.containerRef.nativeElement;
+    let scrollAmount = 0;
+    const slideTimer = setInterval(() => {
+      container.scrollLeft -= container.clientWidth / 12;
+      scrollAmount += container.clientWidth / 12;
+      if (scrollAmount >= container.clientWidth / 2) {
+        window.clearInterval(slideTimer);
+        this.updateArrows();
+      }
+    }, 25);
+  }
+
+  updateArrows() {
+    const el = this.containerRef?.nativeElement;
+    this.showRightArrow = el.scrollWidth - el.scrollLeft - el.clientWidth >= 1;
+    this.showLeftArrow = el.scrollLeft > 0;
+  }
+
   getObjectKeys() {
     return Object.keys(this.gamesListBatches);
   }
--- src/app/pages/home-v2/home-v2.component.ts
+++ src/app/pages/home-v2/home-v2.component.ts
@@ -33,6 +33,9 @@ export class HomeV2Component implements OnInit, OnDestroy {
   public playVideo: boolean = false;
   public isVideoMute: boolean = true;
 
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
+
 
   public firstSignUpMsgTimer: number | null = null;
 
@@ -162,6 +165,24 @@ export class HomeV2Component implements OnInit, OnDestroy {
     clearTimeout(this.bannerShowTimer);
     clearTimeout(this.playVideoTimer);
     Swal.close();
+  }
+
+  swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+
+      if (duration < 1000 && Math.abs(direction[0]) > 30 && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { 
+          const swipe = direction[0] < 0 ? 'RIGHT' : 'LEFT';
+          this.moveSelectedCard(swipe);
+      }
+    }
   }
 
   loadMoreRails() {
