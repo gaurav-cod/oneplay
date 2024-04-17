962015ee100b61d543059fabcc5ef476b663ff6d
maynak <mainak@oneplay.in>
2024-04-17 22:38:32 +0530

    fix spotlight banner video hover

--- src/app/components/game-card-v2/game-card-v2.component.ts
+++ src/app/components/game-card-v2/game-card-v2.component.ts
@@ -152,23 +152,25 @@ export class GameCardV2Component implements AfterViewInit {
   }
 
   mouseEnterHandler() {
-    this.onMouseHoverCard.emit(this.game.oneplayId);
-    setTimeout(()=> {
-      this.showHover = !this.isMobile && !this.game.trailer_video && (this.hoveringCardId == this.game.oneplayId);
-      if (this.showHover) {
-        this.onMouseHoverCard.emit(this.game.oneplayId);
-        
-        setTimeout(()=> {
-          if (this.hoverImage.nativeElement.getBoundingClientRect().left < 0) {
-            this.hoverImage.nativeElement.style.left = String(Number(this.hoverImage.nativeElement.style.left) + 150) + "px";
-          } 
-          if (this.hoverImage.nativeElement.getBoundingClientRect().right > window.innerWidth) {
-            this.hoverImage.nativeElement.style.left = String(Number(this.hoverImage.nativeElement.style.left) - 200) + "px";
-          }
-        }, 50)
+    if (!this.getVideo) {
+      this.onMouseHoverCard.emit(this.game.oneplayId);
+      setTimeout(()=> {
+        this.showHover = !this.isMobile && !this.game.trailer_video && (this.hoveringCardId == this.game.oneplayId);
+        if (this.showHover) {
+          this.onMouseHoverCard.emit(this.game.oneplayId);
           
-      }
-    }, 100)
+          setTimeout(()=> {
+            if (this.hoverImage.nativeElement.getBoundingClientRect().left < 0) {
+              this.hoverImage.nativeElement.style.left = String(Number(this.hoverImage.nativeElement.style.left) + 150) + "px";
+            } 
+            if (this.hoverImage.nativeElement.getBoundingClientRect().right > window.innerWidth) {
+              this.hoverImage.nativeElement.style.left = String(Number(this.hoverImage.nativeElement.style.left) - 200) + "px";
+            }
+          }, 50)
+            
+        }
+      }, 100)
+    }
   }
   mouseLeaveHandler() {
     if (this.hoveringCardId == this.game.oneplayId) {
