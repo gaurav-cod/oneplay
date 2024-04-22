1e41b5107fc859b3c0d6469bb66a86a90eaea2c4
Gaurav Sahu <gaurav.sahu@letsmobility.com>
2024-04-22 22:19:31 +0530

    url changes

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -889,7 +889,7 @@ export class RestService {
       }
     }
     return this.http
-      .post<any[]>(this.r_mix_api_2 + "/games/gamezop/get_filtered_games", data,{
+      .post<any[]>(this.r_mix_api + "/games/gamezop/get_filtered_games", data,{
         params: { page, limit },
       })
       .pipe(
