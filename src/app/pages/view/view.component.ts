76c922362c8c44af55f1f4ca1e8cb812434b47f2
Gaurav Sahu <gaurav.sahu@letsmobility.com>
2024-04-26 18:35:24 +0530

    stream config

--- src/app/pages/view/view.component.ts
+++ src/app/pages/view/view.component.ts
@@ -1708,6 +1708,7 @@ export class ViewComponent implements OnInit, OnDestroy {
   }
 
   openStreamInput(stream: streamConfig) {
+    this.streamErrorMsg = null;
     this.streamConfigList.forEach((s, index)=> {
       s.setIsClicked(s.serviceName == stream.serviceName);
     })
